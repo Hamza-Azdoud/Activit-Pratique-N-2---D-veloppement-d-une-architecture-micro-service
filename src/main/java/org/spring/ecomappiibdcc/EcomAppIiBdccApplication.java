289d2e93b714a74488954d9e1413d98ec26ea6fe
Hamza-Azdoud <h.azdoud@etu.enset-media.ac.ma>
2024-11-27 01:33:32 +0100

    first commit

---
+++ src/main/java/org/spring/ecomappiibdcc/EcomAppIiBdccApplication.java
@@ -0,0 +1,13 @@
+package org.spring.ecomappiibdcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomAppIiBdccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomAppIiBdccApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/ecomappiibdcc/EcomAppIiBdccApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.ecomappiibdcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAppIiBdccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
