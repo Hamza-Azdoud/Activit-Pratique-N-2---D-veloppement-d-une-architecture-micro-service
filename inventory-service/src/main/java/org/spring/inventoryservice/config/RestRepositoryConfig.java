53dfefa763b7b0ed179be3b192de5d1e887471cd
Hamza-Azdoud <h.azdoud@etu.enset-media.ac.ma>
2024-11-27 16:19:37 +0100

    Inventory Service

---
+++ inventory-service/src/main/java/org/spring/inventoryservice/config/RestRepositoryConfig.java
@@ -0,0 +1,16 @@
+package org.spring.inventoryservice.config;
+
+import org.spring.inventoryservice.entities.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestRepositoryConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Product.class);
+
+    }
+}
